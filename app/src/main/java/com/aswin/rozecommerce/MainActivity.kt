501696d71455e6428bcdbe36be8ad9113e119a73
aswinrozario <aswinrozario93@gmail.com>
2025-01-23 15:28:22 +0530

    - base commite

--- app/src/main/java/com/aswin/rozecommerce/MainActivity.kt
+++ app/src/main/java/com/aswin/rozecommerce/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
+
 }
